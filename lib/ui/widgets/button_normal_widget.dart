0859da1179cacb2f83d16d89c8a4a4c09ea5f16c
antony <202100354e@utea.edu.pe>
2024-10-17 01:21:15 -0500

    creaciond e botones

---
+++ lib/ui/widgets/button_normal_widget.dart
@@ -0,0 +1,39 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:timeee/services/my_service_firestore.dart';
+import '../general/colors.dart';
+
+class ButtonNormalWidget extends StatelessWidget {
+
+  Function onPressed;
+
+  ButtonNormalWidget({required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 52.0,
+      width: double.infinity,
+      child: ElevatedButton.icon(
+        onPressed: () {
+          onPressed();
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kBrandPrimaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(14.0),
+          ),
+        ),
+        icon: Icon(Icons.save),
+        label: Text(
+          "Guardar",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
